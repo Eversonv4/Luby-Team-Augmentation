bb65cb8b32e57ebaf7d2cc45b1d88f59f800c483
Everson Soares <eversonv4@gmail.com>
2022-09-26 15:33:13 -0300

    ✨ adding animation on 'stacks' section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { BenefitsSection } from "@sections/benefits";
 import { ThePillars } from "@sections/thePillars";
 import { MultiTeamsSection } from "@sections/multiTeams";
 import { StacksSection } from "@sections/techStacks";
+import { TestimonnialsSection } from "@sections/testimonials";
 
 function Home() {
   return (
@@ -19,6 +20,7 @@ function Home() {
       <ThePillars />
       <MultiTeamsSection />
       <StacksSection />
+      <TestimonnialsSection />
     </>
   );
 }
---
+++ src/sectionsPage/testimonials/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function TestimonnialsSection() {
+  return (
+    <div>
+      <h1 style={{ marginTop: 150 }}>Testimonials</h1>
+    </div>
+  );
+}
